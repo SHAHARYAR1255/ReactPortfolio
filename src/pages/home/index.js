33722b72c41feade328b14749a904da599a8d1a1
shaharyar malik <shaharyar.malik2000@gmail.com>
2022-10-26 14:28:44 +0500

    photos1

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
-import Bhai from './assets/images/bhai.jpg';
+import Bhai from '../../assets/images/bhai.jpg';
 
 
 export const Home = () => {
